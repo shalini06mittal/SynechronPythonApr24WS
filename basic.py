e08d37b4a04d3c452a6a16e8113c1432b5eaaae0
Shalini Mittal <shalini06.mittal@gmail.com>
2024-04-06 10:17:33 +0530

    first commit

---
+++ basic.py
@@ -0,0 +1,20 @@
+'''
+spaces or indendation is VVIMP
+No ; and No {} to mark a block of code
+block of code is marked with a : and space
+'''
+print(2+3);             print               (               'hello')
+print               (               "hello there")
+print(''' hello
+      there
+      welcome''')
+'''
+used for documentation/ comments
+triple quotes are used for multi line comments and # is used for singlr line comment
+'''
+# single line comment
+# Hey "Mikey", Let's go, escape sequence - that tells python to escape the original meaning of 
+# a character and print it as it is
+# backslash followed by a single character without a space \n \t
+print('Hey "Mikey", Let\'s\t go')
+print("Hey \"Mikey\", Let's\n go")
