a48a4f2f89aa160aa26ccc04daa532332cce79a8
Shalini Mittal <shalini06.mittal@gmail.com>
2024-04-07 10:48:50 +0530

    straing data structure

---
+++ datastructures.py
@@ -0,0 +1,30 @@
+'''
+structure to store data in a certain format  based on the use case
+
+immutable
+string - supports indexing Shalini index - 0
+store values using '' or "" or ''' ''' or """ """
+cannot modify once created
+methods - 
+
+tuple -supports indexing, index - 0
+store values using () or , or tuple()
+tuples are fixed and cannot modify the value: array + enum
+methods - 
+
+methods - specific to a structure 
+and functions - gobal can be used across structure
+
+mutable
+list - index 0, [] or list()
+allows duplicates
+methods
+
+set - does not support index [ storage is dynamic], {} or set()
+not allows duplicates
+methods
+
+dictionary - key-value, {} or dict()
+allows duplicate values but not duplicate keys
+methods
+'''
---
+++ strings.py
@@ -0,0 +1,38 @@
+msg='Welcome Guest'
+print(len(msg))
+print(msg.count('e'))
+print(msg.endswith('Guest'))
+print(msg.upper())
+print(msg.lower())
+
+print('index',msg.index('e'))
+print(msg.index('e', 2))
+# username -   shalini  
+#dbusername = shalini
+desc = '     some     description     '
+print('***'+desc+'***')
+print('***'+desc.rstrip()+'***')
+print('***'+desc.lstrip()+'***')
+print('***'+desc.strip()+'***')
+
+print('Hey {0} and heres you lucky number{1}'.format(msg, 5))
+# slicing 
+# http://
+url ='http://www.techgatha.com'
+# start:stop:step
+print(url[0:7]) # http://
+print(url[7:])# www.techgatha.com
+print(url[:7])# http://
+print(url[5:])# //www.techgatha.com
+print(url[0:len(url):5])# h/.g.
+print(msg[::-1])# tseuG emocleW
+
+print('1',msg[len(msg)::-1])
+# print(msg[len(msg)])
+
+# separate the name and domain name
+email='shalini@gmail.com'
+
+# the format of file location does not change. 
+#from the file location print the name and extension of image seaparate.
+fileloc = 'file://C:/images/pic1.jpg'
