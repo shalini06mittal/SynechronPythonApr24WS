a180f1ab3b5783abbb90e7c8c56ef42e2231b191
Shalini Mittal <shalini06.mittal@gmail.com>
2024-04-13 15:08:17 +0530

    json

--- programs/filehandling/jsonreadwrite.py
+++ programs/filehandling/jsonreadwrite.py
@@ -1,4 +1,4 @@
-person = '{"fname":"shalini","languages":["english","hindi"]}'
+person = '{"fname":"shalini","languages":["english","hindi"],"address":[{"city":"C1","street":{"street1":"some street"}},{"city":"C2"}]}'
 
 import json
 person_dict = json.loads(person)
@@ -7,13 +7,13 @@ print(person_dict)
 print(person_dict['languages'])
 
 # with open('person.txt','w') as file:
-#     json.dump(person_dict, file)
+#     json.dump(person_dict, file) # will actually write the content of dict to a file
 
 with open('person.json','w') as file:
-    json.dump(person_dict, file,indent=2)
+    json.dump(person_dict, file,indent=4)
 
 with open('student.json') as file:
     data = json.load(file)
 
 print(data)
-print(json.dumps(person_dict))
+print(json.dumps(person_dict)) # converts python dict to json
