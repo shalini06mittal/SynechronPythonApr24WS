62f579a362159c2142c52d223b57ad7ab3324040
Shalini Mittal <shalini06.mittal@gmail.com>
2024-04-13 14:58:33 +0530

    json

---
+++ programs/filehandling/jsonreadwrite.py
@@ -0,0 +1,16 @@
+person = '{"fname":"shalini","languages":["english","hindi"]}'
+
+import json
+person_dict = json.loads(person)
+
+print(person_dict)
+print(person_dict['languages'])
+
+# with open('person.txt','w') as file:
+#     json.dump(person_dict, file)
+
+with open('student.json') as file:
+    data = json.load(file)
+
+print(data)
+print(json.dumps(person_dict))
