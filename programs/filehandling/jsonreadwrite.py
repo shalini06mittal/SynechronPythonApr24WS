5a704f9e948768abcde624363b6603d067862d73
Shalini Mittal <shalini06.mittal@gmail.com>
2024-04-13 15:01:58 +0530

    json

--- programs/filehandling/jsonreadwrite.py
+++ programs/filehandling/jsonreadwrite.py
@@ -9,6 +9,9 @@ print(person_dict['languages'])
 # with open('person.txt','w') as file:
 #     json.dump(person_dict, file)
 
+with open('person.json','w') as file:
+    json.dump(person_dict, file,indent=2)
+
 with open('student.json') as file:
     data = json.load(file)
 
